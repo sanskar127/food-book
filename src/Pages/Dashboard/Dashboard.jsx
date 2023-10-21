6f2e58e53bc62e4123fe199cdeadb5efd9ad6c4d
Sanskar Pandey <sanskarpandey281@gmail.com>
2023-10-21 23:44:42 +0530

    more update

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -2,12 +2,30 @@ import React from 'react'
 import './dashboard.css'
 import Sidebar from '../../Components/Sidebar/Sidebar'
 import Navbar from '../../Components/Navbar/Navbar'
+import Circle from '../../assets/images/add-circle.svg'
 
 const Dashboard = () => {
   return (
     <div className='dashboard'>
-      <Sidebar/>
-      <Navbar/>
+
+      <section>
+        <Navbar />
+      </section>
+
+      <section>
+        <Sidebar />
+      </section>
+
+      <section className='main'>
+        <div className="header">
+          <div>
+            <h2 className="heading">Your Campaigns</h2>
+            <h4 className="subheading">Check the list of campigns you created </h4>
+          </div>
+          <button className="create"> <img src={Circle} alt="plus" /> Create new campaign</button>
+        </div>
+      </section >
+
     </div>
   )
 }
